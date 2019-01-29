0de730b160909ba86269ccc24949465982b60941
osandoval1976 <oscarhs0576@gmail.com>
2019-01-28 20:24:33 -0400

    github web
    js

---
+++ js/js.js
@@ -0,0 +1,19 @@
+$( "#name" ).focus();
+
+$("document").ready(function(){
+	$("#other-title").hide();
+	$("#title").click(function(){
+
+	
+		$("#title").click(function(){
+		$("#title").val("other");
+		$("#other-title").show();
+		
+	});	
+		
+	
+});
+	
+	});
+
+	
