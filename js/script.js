b1e97723d4b9bb3a81d92c6d6cf4615c38de373c
osandoval1976 <oscarhs0576@gmail.com>
2019-01-31 11:18:22 -0400

    script files uploaded web

---
+++ js/script.js
@@ -0,0 +1,19 @@
+$( "#name" ).focus();
+
+var x = document.getElementById("title").options[5];
+
+var z = document.getElementById("other-title");
+ var y = document.getElementById("other-title").hidden=true;
+  x.addEventListener('click', ()=>{
+   y.hidden = true;
+   
+});
+
+var z = document.getElementById("other-title");
+  x.addEventListener('click', ()=>{
+   y.hidden = false;
+   
+});
+
+
+
